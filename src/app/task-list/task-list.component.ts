f85a4fed067354c13602a6c2435249226e808f54
JugurtaO <jugurtaourzik@gmail.com>
2024-02-23 21:45:21 +0100

    +master: added confirmation message to delete task

--- src/app/task-list/task-list.component.ts
+++ src/app/task-list/task-list.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, OnInit, computed, numberAttribute, signal } from '@angular/core';
+import {  Component, OnInit, computed, numberAttribute, signal } from '@angular/core';
 import { Task } from '../Models/task';
 import { NgFor } from '@angular/common';
 import { TaskService } from '../services/task.service';
@@ -9,20 +9,24 @@ import { MatIcon } from '@angular/material/icon';
 import { ButtonModule } from 'primeng/button';
 import { CarouselModule, } from 'primeng/carousel';
 import { CardModule } from 'primeng/card';
+import { ConfirmPopupModule } from 'primeng/confirmpopup';
 import { BadgeModule } from 'primeng/badge';
+import { ConfirmationService, MessageService } from 'primeng/api';
+import { ToastModule } from 'primeng/toast';
 @Component({
   selector: 'app-task-list',
   standalone: true,
-  imports: [NgFor, RouterOutlet, AddTaskComponent, NavbarComponent, MatIcon,CarouselModule,ButtonModule,CardModule,BadgeModule],
+  imports: [NgFor, RouterOutlet, AddTaskComponent, NavbarComponent, MatIcon,CarouselModule,ButtonModule,CardModule,BadgeModule,ConfirmPopupModule,ToastModule],
   templateUrl: './task-list.component.html',
-  styleUrl: './task-list.component.css'
+  styleUrl: './task-list.component.css',
+  providers:[ConfirmationService,MessageService]
 })
 export class TaskListComponent implements OnInit {
 
 
 
 
-  constructor(private taskService: TaskService, private router: Router) { }
+  constructor(private taskService: TaskService, private router: Router,private confirmationService: ConfirmationService, private messageService: MessageService) { }
 
   public tasks: Task[] = [];
   ngOnInit(): void {
@@ -61,6 +65,20 @@ export class TaskListComponent implements OnInit {
 
 
 
-
+  confirmDeleteTask(event:Event,task:Task) {
+    this.confirmationService.confirm({
+        target: event.target as EventTarget,
+        message: 'Do you want to delete this task?',
+        icon: 'pi pi-info-circle',
+        acceptButtonStyleClass: 'p-button-danger p-button-sm',
+        accept: () => {           
+            this.deleteTask(task.id);
+            this.messageService.add({ severity: 'info', summary: 'Confirmed', detail: `task n°${task.id} successfully deleted`, life: 3000 });
+        },
+        reject: () => {
+            this.messageService.add({ severity: 'error', summary: 'Rejected', detail: 'You have rejected', life: 3000 });
+        }
+    });
+}
 
 }
